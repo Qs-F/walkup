af622b4e738962e3ca8d59a877adcb71928bd2fc
Qs-F <qsf@de-liker.com>
2018-07-12 22:14:19 -0400

    Add parent and walkList tests

--- walkup_test.go
+++ walkup_test.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/Qs-F/mintest"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,10 +15,11 @@ func TestWalkup(t *testing.T) {
 		t.Error(err.Error())
 	}
 
-	err = os.Chdir(filepath.Join(dir, "A", "B"))
+	err = os.Chdir(filepath.Join(dir, "_testdata", "A", "B"))
 	if err != nil {
 		t.Error(err.Error())
 	}
+	defer os.Chdir(dir)
 
 	current, err := os.Getwd()
 	if err != nil {
@@ -25,12 +27,78 @@ func TestWalkup(t *testing.T) {
 	}
 
 	// Walkup( /* basedir */, /* filename */, /* directory level (0 means to root dir) */ )
-	filelist, err := Walkup(current, "TEMP", 0)
+	filelist, err := Walkup(filepath.Join(current, "_testdata"), "TEMP", 0)
 	if err != nil {
 		t.Error(err.Error())
 	}
 
-	if !assert.Equal(t, filelist, []string{filepath.Join(dir, "TEMP")}) {
+	if !assert.Equal(t, filelist, []string{filepath.Join(dir, "_testdata", "TEMP")}) {
 		t.Error("Files does not match")
+	}
+}
+
+func TestParent(t *testing.T) {
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	base := filepath.Join(dir, "_testdata", "A", "B")
+
+	if parent(base) != filepath.Join(dir, "_testdata", "A") {
+		t.Error("Not parent directory")
+	} else {
+		t.Log(parent(base))
+	}
+}
+
+func TestWalkList(t *testing.T) {
+	type test struct {
+		Dir string
+		N   int
+	}
+	tests := mintest.TestSlices{
+		{
+			Input: test{
+				Dir: "/_testdata/A/B",
+				N:   0,
+			},
+			Must: []string{
+				"/_testdata/A/B",
+				"/_testdata/A",
+				"/_testdata",
+				"/",
+			},
+			Error: true,
+		},
+		{
+			Input: test{
+				Dir: "/_testdata/A/B/C/D",
+				N:   2,
+			},
+			Must: []string{
+				"/_testdata/A/B/C/D",
+				"/_testdata/A/B/C",
+			},
+			Error: true,
+		},
+		{
+			Input: test{
+				Dir: "/_testdata/A/B/C/D",
+				N:   5,
+			},
+			Must: []string{
+				"/_testdata/A/B/C/D",
+				"/_testdata/A/B/C",
+				"/_testdata/A/B",
+				"/_testdata/A",
+				"/_testdata",
+				"/",
+			},
+			Error: true,
+		},
+	}
+
+	for n := 0; n < len(tests); n++ {
+		assert.Equal(t, tests[n].Must, walkList(tests[n].Input.(test).Dir, tests[n].Input.(test).N))
 	}
 }
